9167244ba7db9b410e2a2a7bc7c45a5b8ea69c8f
sonephet <sonephet.dev@gmail.com>
2023-05-21 12:25:33 +0700

    update image path

--- layouts/default.vue
+++ layouts/default.vue
@@ -36,5 +36,9 @@ a {
   justify-content: space-between;
   box-shadow: 0 0 5px 5px #ddd;
   padding: 15px 30px;
+  position: sticky;
+  top: 0;
+  z-index: 999999;
+  background-color: #fff;
 }
 </style>
